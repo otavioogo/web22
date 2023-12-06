bc096d3dd4dd441c10f69dc7b60f5e504f4dde15
Otavio Ogo <otavioeidi@hotmail.com>
2023-12-06 16:51:20 -0300

    Estrutura Inicial + conectando BD

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Ola mundo')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI,{
+}, console.log('Conectado Banco de Dados'))
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req , res ) => {
+    res.send('Ola Mundo')
+})
+
+app.listen(8000, () => console.log('Server Rodando'))
